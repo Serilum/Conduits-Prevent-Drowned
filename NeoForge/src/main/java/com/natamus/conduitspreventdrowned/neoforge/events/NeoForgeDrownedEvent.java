4ca851e56bc939ee0e5970b6ef32bf9c233b7cd0
Rick South <rick@natamus.com>
2024-05-26 14:57:25 +0200

    (v3.7) (1.20.6 Neo) Updated mod to work with NeoForge version 20.6.78+.

--- NeoForge/src/main/java/com/natamus/conduitspreventdrowned/neoforge/events/NeoForgeDrownedEvent.java
+++ NeoForge/src/main/java/com/natamus/conduitspreventdrowned/neoforge/events/NeoForgeDrownedEvent.java
@@ -3,30 +3,22 @@ package com.natamus.conduitspreventdrowned.neoforge.events;
 import com.natamus.collective.functions.WorldFunctions;
 import com.natamus.conduitspreventdrowned.events.DrownedEvent;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.entity.living.MobSpawnEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.MobSpawnEvent;
 
 @EventBusSubscriber
 public class NeoForgeDrownedEvent {
 	@SubscribeEvent
-	public static void onDrownedSpawn(MobSpawnEvent.FinalizeSpawn e) {
+	public static void onDrownedSpawn(MobSpawnEvent.PositionCheck e) {
 		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
 		if (level == null) {
 			return;
 		}
-		
-		Entity entity = e.getEntity();
-		if (!(entity instanceof Mob)) {
-			return;
-		}
-		
-		if (!DrownedEvent.onDrownedSpawn((Mob)entity, (ServerLevel)level, null, e.getSpawnType())) {
-			e.setSpawnCancelled(true);
-			e.setCanceled(true);
+
+		if (!DrownedEvent.onDrownedSpawn(e.getEntity(), (ServerLevel)level, null, e.getSpawnType())) {
+			e.setResult(MobSpawnEvent.PositionCheck.Result.FAIL);
 		}
 	}
 }
