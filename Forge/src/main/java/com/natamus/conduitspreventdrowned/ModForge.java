0cbf25652707e622c6eb12edc1cec14a3c094397
Rick South <rick@natamus.com>
2024-07-28 07:39:53 +0200

    (v3.8) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/conduitspreventdrowned/ModForge.java
+++ Forge/src/main/java/com/natamus/conduitspreventdrowned/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.conduitspreventdrowned;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.conduitspreventdrowned.forge.config.IntegrateForgeConfig;
 import com.natamus.conduitspreventdrowned.forge.events.ForgeDrownedEvent;
 import com.natamus.conduitspreventdrowned.util.Reference;
@@ -15,6 +16,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
