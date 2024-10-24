60bc1fe79ad874106aa9212c67a49630a7ee0eba
Rick South <rick@natamus.com>
2024-10-24 20:09:33 +0200

    Conduits Prevent Drowned source code for 1.21.2.

---
+++ NeoForge/src/main/java/com/natamus/conduitspreventdrowned/ModNeoForge.java
@@ -0,0 +1,39 @@
+package com.natamus.conduitspreventdrowned;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.conduitspreventdrowned.neoforge.config.IntegrateNeoForgeConfig;
+import com.natamus.conduitspreventdrowned.neoforge.events.NeoForgeDrownedEvent;
+import com.natamus.conduitspreventdrowned.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateNeoForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeDrownedEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
