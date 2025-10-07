5be07928886fba8716ace5a30549624cc745a084
Rick South <rick@natamus.com>
2025-10-08 01:46:01 +0200

    Conduits Prevent Drowned source code for 1.21.10.

---
+++ Forge/src/main/java/com/natamus/conduitspreventdrowned/forge/events/ForgeDrownedEvent.java
@@ -0,0 +1,40 @@
+package com.natamus.conduitspreventdrowned.forge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.conduitspreventdrowned.events.DrownedEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.living.MobSpawnEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeDrownedEvent {
+	public static void registerEventsInBus() {
+		// BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeDrownedEvent.class);
+
+		MobSpawnEvent.FinalizeSpawn.BUS.addListener(ForgeDrownedEvent::onDrownedSpawn);
+	}
+
+	@SubscribeEvent
+	public static boolean onDrownedSpawn(MobSpawnEvent.FinalizeSpawn e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return false;
+		}
+		
+		Entity entity = e.getEntity();
+		if (!(entity instanceof Mob)) {
+			return false;
+		}
+		
+		if (!DrownedEvent.onDrownedSpawn((Mob)entity, (ServerLevel)level, null, e.getSpawnReason())) {
+			e.setSpawnCancelled(true);
+			return true;
+		}
+		return false;
+	}
+}
